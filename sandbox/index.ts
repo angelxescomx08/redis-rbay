3e7b9881ed098247bd2f88d0052832d8b5e75f75
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-08-25 14:07:45 -0600

    Implementar funciones para establecer y obtener datos de un objeto "car" en Redis. Se agregó la lógica para manejar la creación y recuperación de un coche, incluyendo un mensaje para el caso en que no se encuentre el coche.

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,21 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+  await client.hSet("car", {
+    color: "red",
+    year: 1950,
+    engine: { cylinders: 8 },
+    owner: null || "",
+    services: undefined || ""
+  })
+
+  const car = await client.hGetAll("car#fds")
+
+  if (Object.keys(car).length === 0) {
+    console.log("Not found")
+  }
+
+  console.log(car)
+};
 run();
